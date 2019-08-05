512d66c771d7c4a19905a1c337dd051837ade2bf
brandnn <brandnn@uw.edu>
2019-08-05 11:54:11 -0700

    do not approve

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,17 +1,46 @@
 
 //make a city search and then take those results and pass them into 
-//var queryURL = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?key=AIzaSyCPnrEUe-GDsavDjTaLAaVR8bKZ15QOTVc&"
-var queryautocom = "https://maps.googleapis.com/maps/api/place/autocomplete/json?key=AIzaSyCPnrEUe-GDsavDjTaLAaVR8bKZ15QOTVc&offset=3&input=Seattle";
+var originalURL = "https://maps.googleapis.com/maps/api/place/autocomplete/json?key=AIzaSyCPnrEUe-GDsavDjTaLAaVR8bKZ15QOTVc&offset=3&input=Seattle";
+var secondURL = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?key=AIzaSyCPnrEUe-GDsavDjTaLAaVR8bKZ15QOTVc&";
+var queryautocom = "https://cors-anywhere.herokuapp.com/" + secondURL;
+var queryURL = "https://cors-anywhere.herokuapp.com/" + originalURL;
+
+var map;
+var latitude = 47.6062
+var long = -122.3321
+
+console.log(queryautocom)
+console.log(queryURL)
 
 $.ajax({
-  crossOrigin: true,
+  url: queryURL,
+  method: "GET",
+  dataType: "json",
+  // this headers section is necessary for CORS-anywhere
+  headers: {
+    "x-requested-with": "xhr" 
+  },
+  success: function(data) {
+    timestamp = data.results;
+    console.log(timestamp);
+},
+error: function() {
+    console.log("aw crap");
+}
+})
+
+
+$.ajax({
   url: queryautocom,
   method: "GET",
-  dataType: 'jsonp'
+  dataType: "json",
+  // this headers section is necessary for CORS-anywhere
+  headers: {
+    "x-requested-with": "xhr" 
+  }
 
  }).then(function(responseautocom) {
-
-  autocomoptions = responseautocom.results;
+  var autocomoptions = responseautocom.results;
   console.log(autocomoptions);
  });
 
@@ -29,18 +58,14 @@ $.ajax({
 //    });
 
 
-
 //Google Map Call 
-var map;
-var latitude = 47.6062
-var long = -122.3321
 
 
 
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: latitude, lng: long},
-    zoom: 10  
-  });
-}
+    zoom: 16  
+  })
+};
 
