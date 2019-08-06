d63b96810bd2d7007fab7a92f55b0a01ca16f0d1
brandnn <brandnn@uw.edu>
2019-08-05 19:33:14 -0700

    added city search capabilities as well as being able to search by hitting enter key

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,7 +1,7 @@
 
 //make a city search and then take those results and pass them into 
 var originalURL = "https://maps.googleapis.com/maps/api/place/autocomplete/json?key=AIzaSyCPnrEUe-GDsavDjTaLAaVR8bKZ15QOTVc&offset=4&input=Seattle";
-var secondURL = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?key=AIzaSyCPnrEUe-GDsavDjTaLAaVR8bKZ15QOTVc&";
+var secondURL = "https://api.opencagedata.com/geocode/v1/json?q=Portland&key=ce4024be27f7473587cd9b456f635db5;"
 var queryautocom = "https://cors-anywhere.herokuapp.com/" + secondURL;
 var queryURL = "https://cors-anywhere.herokuapp.com/" + originalURL;
 
@@ -29,42 +29,86 @@ error: function() {
 }
 })
 
+$("#search-name").on('keypress',function(e) {
+  if(e.which == 13) {
+    var search = $("#search-name").val();
+    console.log(search)
+    secondURL = "https://api.opencagedata.com/geocode/v1/json?q=" + search +"&key=ce4024be27f7473587cd9b456f635db5";
+    queryautocom = "https://cors-anywhere.herokuapp.com/" + secondURL;
+  
+    $.ajax({
+    url: queryautocom,
+    method: "GET",
+    dataType: "json",
+    // this headers section is necessary for CORS-anywhere
+    headers: {
+      "x-requested-with": "xhr" 
+    },
+    success: function(data1) {
+      timestamp = data1;
+      console.log(timestamp);
+      resultlong = data1.results[0].geometry.lng;
+      console.log(resultlong);
+      longitude = resultlong
+      resultlat = data1.results[0].geometry.lat
+      console.log(resultlat)
+      latitude = resultlat
+      map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: latitude, lng: longitude},
+        zoom: 10  
+      })
+    },
+  error: function() {
+      console.log("aw crap");
+  }
+  
+   })
+  
 
-$.ajax({
+  }
+});
+
+
+$("#search").on("click", function() {
+  var search = $("#search-name").val();
+  console.log(search)
+  secondURL = "https://api.opencagedata.com/geocode/v1/json?q=" + search +"&key=ce4024be27f7473587cd9b456f635db5";
+  queryautocom = "https://cors-anywhere.herokuapp.com/" + secondURL;
+
+  $.ajax({
   url: queryautocom,
   method: "GET",
   dataType: "json",
   // this headers section is necessary for CORS-anywhere
   headers: {
     "x-requested-with": "xhr" 
-  }
+  },
+  success: function(data1) {
+    timestamp = data1;
+    console.log(timestamp);
+    resultlong = data1.results[0].geometry.lng;
+    console.log(resultlong);
+    longitude = resultlong
+    resultlat = data1.results[0].geometry.lat
+    console.log(resultlat)
+    latitude = resultlat
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: latitude, lng: longitude},
+      zoom: 10  
+    })
+  },
+error: function() {
+    console.log("aw crap");
+}
 
- }).then(function(responseautocom) {
-  var autocomoptions = responseautocom.items;
-  console.log(autocomoptions);
- });
-
-
-
-
-// $.ajax({
-//     crossOrigin: true,
-//     url: queryURL,
-//     method: "GET"
-//    }).then(function(response) {
-
-//    options = response.results;
-//     console.log(options);
-//    });
-
-
-//Google Map Call 
+ })
+});
 
 
 
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: latitude, lng: longitude},
-    zoom: 16  
+    zoom: 10  
   })
 };
