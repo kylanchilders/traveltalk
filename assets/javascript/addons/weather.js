ff1d3cbe4a623fc655a6f81ba9691126ac39d408
T3k5upp0rt <kylan.childers@gmail.com>
2019-08-10 12:52:27 -0700

    created javascript to aggregate ratings using firebase. Created accuweather api call javascript

--- assets/javascript/addons/weather.js
+++ assets/javascript/addons/weather.js
@@ -1,14 +1,31 @@
-var queryURL = "http://dataservice.accuweather.com/currentconditions/v1/351409?apikey=y19ljE9enoIiYwNP3GWqpi7jyPEYrldx&language=en-us&details=false"
+var queryURL = "http://dataservice.accuweather.com/currentconditions/v1/"
+
+var apiKey = "?apikey=y19ljE9enoIiYwNP3GWqpi7jyPEYrldx&language=en-us&details=false"
+
+var Cities = {
+    London: "328328",
+    Paris: "623",
+    Seattle: "351409",
+    SanFrancisco: "347629",
+    NewYork: "349727",
+    HongKong: "1123655",
+    Johannesburg: "305448",
+    Perth: "26797",
+    Madrid: "308526",
+    Greece: "182536"
+};
+
+const locationKeys = Object.values(Cities)
 
 
-
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(response){
-    console.log(response)
-    console.log(response[0].WeatherText);
-
-    $("#weatherInfo").append("<div>" + response.WeatherText);
-});
+for (i = 0; i < locationKeys.length; i++){
+    currentURL = queryURL + locationKeys[i] + apiKey;
+    
+    $.ajax({
+        url: currentURL,
+        method: "GET"
+    }).then(function(response){
+        $("#weatherInfo").append("<div>" + "Conditions: " + response[0].WeatherText + "</div><br><div>Temperature: " + response[0].Temperature.Imperial.Value + "</div>")
+    });
+}
 
