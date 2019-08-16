ec7a8498c08c13c4cc907b729b9dc0bb0dd0813a
T3k5upp0rt <kylan.childers@gmail.com>
2019-08-16 15:54:40 -0700

    Added responsive rating stars to maps.html as well as created modals for more info on maps.html

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -47,30 +47,152 @@ function eventbrite() {
 
     var eventId = event.id;
     var template = `
-      <li id="eventCard##EVENT-ID##" class="card" style="width: 18rem; order: 1" tabindex="-1" class="uk-active">
-          <img id="eventPhoto##EVENT-ID##" class="card-img-top" src="" alt="Card image cap" style="height: 165px;">
+      <li class="card eventCard##EVENT-ID##" style="width: 18rem; order: 1" tabindex="-1" class="uk-active">
+          <img class="card-img-top eventPhoto##EVENT-ID##" src="" alt="Card image cap" style="height: 165px;">
           <div class="card-body">
-              <h4 id="eventTitle##EVENT-ID##" style="text-align: center"></h4>
-              <h6>Date of Event: <span id="eventDate##EVENT-ID##"></span></h6>
-              <p id="eventDescription##EVENT-ID##" class="card-text"></p>
+              <h4 class="eventTitle##EVENT-ID##" style="text-align: center"></h4>
+              <h6>Date of Event: <span class="eventDate##EVENT-ID##"></span></h6>
               <br>
+              <div class="ratingArea##EVENT-ID##">
+                <p style="margin: 0;">Venue Rating:</p>
+                <img src="./assets/images/star-icon-empty.png" class="1star##EVENT-ID##" style="height: 20px; width: 20px;" onclick="1star('##EVENT-ID##'); return false;">
+                <img src="./assets/images/star-icon-empty.png" class="2star##EVENT-ID##" style="height: 20px; width: 20px;">
+                <img src="./assets/images/star-icon-empty.png" class="3star##EVENT-ID##" style="height: 20px; width: 20px;">
+                <img src="./assets/images/star-icon-empty.png" class="4star##EVENT-ID##" style="height: 20px; width: 20px;">
+                <img src="./assets/images/star-icon-empty.png" class="5star##EVENT-ID##" style="height: 20px; width: 20px;">
+              </div>
               <div class="row text-center">
                   <a id="addEventButton##EVENT-ID##" href="#" class="btn btn-primary" style="margin: 10px">+</a>
                   <a id="removeEventButton##EVENT-ID##" href="#" class="btn btn-primary" style="margin: 10px">-</a>
-                  <a id="moreInfoButton##EVENT-ID##" target="sblank" href="#" class="btn btn-primary" style="margin: 10px">More Info</a>
+                  <a id="moreInfoButton##EVENT-ID##" data-toggle="modal" data-target="#exampleModalLong##EVENT-ID##" class="btn btn-primary" style="margin: 10px">More Info</a>
               </div>
           </div>
       </li>
   `;
+
+    var modalTemplate = `
+    <div class="modal fade" id="exampleModalLong##EVENT-ID##" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLongTitle"></h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+      <div class="modal-body">
+      <img class="card-img-top eventPhoto##EVENT-ID##" src="" alt="Card image cap" style="height: 165px; width: 300px; display: block; margin-left: auto; margin-right: auto;">
+      <div class="card-body">
+          <h4 class="eventTitle##EVENT-ID##" style="text-align: center"></h4>
+          <h6 style="margin:0;">Date of Event: <span class="eventDate##EVENT-ID##"></span></h6>
+          <h6 style="margin:0;">Location: <span class="venueName##EVENT-ID##"></span></h6>
+          <h6 style="margin:0;"><span class="venueAddress1##EVENT-ID##"></span></h6>
+          <h6 style="margin:0;"><span class="venueAddress2##EVENT-ID##"></span></h6>
+          <p class="card-text fullEventDescription##EVENT-ID##"></p>
+          </div>
+          <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+      </div>
+  </div>
+</div>
+</div>
+`;
+
     template = template.replace(/##EVENT-ID##/g, eventId);
+    modalTemplate = modalTemplate.replace(/##EVENT-ID##/g, eventId);
+
+    $("body").after(modalTemplate);
+
     $(".uk-slider-items").append(template);
 
 
-    $("#eventPhoto" + eventId).attr("src", event.logo.original.url);
-    $("#eventTitle" + eventId).text(event.name.text);
-    $("#eventDate" + eventId).text(moment(event.start.local).format('MMMM Do YYYY, h:mm a'));
-    $("#eventDescription" + eventId).text(event.description.text.substring(0, 150) + "...");
-    $("#moreInfoButton" + eventId).attr("href", event.url);
+    $(".eventPhoto" + eventId).attr("src", event.logo.original.url);
+    $(".eventTitle" + eventId).text(event.name.text);
+    $(".eventDate" + eventId).text(moment(event.start.local).format('MMMM Do YYYY, h:mm a'));
+    $(".eventDescription" + eventId).text(event.description.text.substring(0, 150) + "...");
+    $(".moreInfoButton" + eventId).attr("href", event.url);
+    $(".fullEventDescription" + eventId).text(event.description.text);
+    $(".venueName" + eventId).text(event.venue.name);
+    $(".venueAddress1" + eventId).text(event.venue.address.address_1);
+    $(".venueAddress2" + eventId).text(event.venue.address.city + ", " + event.venue.address.region + " " + event.venue.address.postal_code);
+
+    //Rating System//
+
+    var userRating
+    var numberOfRatings
+    var initialRating
+
+    $(".1star" + eventId).on("click", function(){
+      $(".1star" + eventId).attr("src", "./assets/images/star-icon-full.png");
+      $(".2star" + eventId).attr("src", "./assets/images/star-icon-empty.png");
+      $(".3star" + eventId).attr("src", "./assets/images/star-icon-empty.png");
+      $(".4star" + eventId).attr("src", "./assets/images/star-icon-empty.png");
+      $(".5star" + eventId).attr("src", "./assets/images/star-icon-empty.png");
+      userRating = 1;
+      numberOfRatings++;
+      $(".ratingArea" + eventId).delay(1500).queue(function(n){
+        $(".ratingArea" + eventId).html("<p>Thanks for your rating!</p>");
+        n();
+      });
+    });
+
+    $(".2star" + eventId).click(function(){
+      $(".1star" + eventId).attr("src", "./assets/images/star-icon-full.png");
+      $(".2star" + eventId).attr("src", "./assets/images/star-icon-full.png");
+      $(".3star" + eventId).attr("src", "./assets/images/star-icon-empty.png");
+      $(".4star" + eventId).attr("src", "./assets/images/star-icon-empty.png");
+      $(".5star" + eventId).attr("src", "./assets/images/star-icon-empty.png");
+      userRating = 2;
+      numberOfRatings++;
+      $(".ratingArea" + eventId).delay(1500).queue(function(n){
+        $(".ratingArea" + eventId).html("<p>Thanks for your rating!</p>");
+        n();
+      });
+    });
+
+    $(".3star" + eventId).click(function(){
+      $(".1star" + eventId).attr("src", "./assets/images/star-icon-full.png");
+      $(".2star" + eventId).attr("src", "./assets/images/star-icon-full.png");
+      $(".3star" + eventId).attr("src", "./assets/images/star-icon-full.png");
+      $(".4star" + eventId).attr("src", "./assets/images/star-icon-empty.png");
+      $(".5star" + eventId).attr("src", "./assets/images/star-icon-empty.png");
+      userRating = 3;
+      numberOfRatings++;
+      $(".ratingArea" + eventId).delay(1500).queue(function(n){
+        $(".ratingArea" + eventId).html("<p>Thanks for your rating!</p>");
+        n();
+      });
+    });
+
+    $(".4star" + eventId).click(function(){
+      $(".1star" + eventId).attr("src", "./assets/images/star-icon-full.png");
+      $(".2star" + eventId).attr("src", "./assets/images/star-icon-full.png");
+      $(".3star" + eventId).attr("src", "./assets/images/star-icon-full.png");
+      $(".4star" + eventId).attr("src", "./assets/images/star-icon-full.png");
+      $(".5star" + eventId).attr("src", "./assets/images/star-icon-empty.png");
+      userRating = 4;
+      numberOfRatings++;
+      $(".ratingArea" + eventId).delay(1500).queue(function(n){
+        $(".ratingArea" + eventId).html("<p>Thanks for your rating!</p>");
+        n();
+      });
+    });
+
+    $(".5star" + eventId).click(function(){
+      $(".1star" + eventId).attr("src", "./assets/images/star-icon-full.png");
+      $(".2star" + eventId).attr("src", "./assets/images/star-icon-full.png");
+      $(".3star" + eventId).attr("src", "./assets/images/star-icon-full.png");
+      $(".4star" + eventId).attr("src", "./assets/images/star-icon-full.png");
+      $(".5star" + eventId).attr("src", "./assets/images/star-icon-full.png");
+      userRating = 5;
+      numberOfRatings++;
+      $(".ratingArea" + eventId).delay(1500).queue(function(n){
+        $(".ratingArea" + eventId).html("<p>Thanks for your rating!</p>");
+        n();
+      });
+    });
+
   };
 
   function getEventByLatLong(latitude, longitude) {
@@ -150,6 +272,12 @@ $("#search-name").on('keyup', function (e) {
 $("#search").on("click", function () {
   google()
 });
+
+//RatingSystem//
+
+
+
+//Rating End//
 
 $body = $("body");
 
