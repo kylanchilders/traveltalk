b018e4a2e691d933d42524dc73f96a41ba59d1ec
Maci Slenes <maci.slenes@gmail.com>
2019-08-16 15:53:34 -0700

    added bookmarks code as well as remove button functions

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,17 +4,17 @@
 var map;
 //var latitude = 47.6062 
 //var longitude = -122.3321
-var latitude = parseFloat(localStorage.getItem("latitude"))
-var longitude = parseFloat(localStorage.getItem("longitude"))
-var numOfEventsToDisplay = 10
+var latitude = parseFloat(localStorage.getItem("latitude"));
+var longitude = parseFloat(localStorage.getItem("longitude"));
+var numOfEventsToDisplay = 10;
 var calcLong = 0;
 var calcLat = 0;
 
 var auth = "S5UUTS2NYPECCKBYF5JY";
-var bookmark = [];
-function addEventCard(event){
-  
+var bookmarks = [];
+var events = [];
 
+function addEventCard(event){
   var eventId = event.id;
   var template = `
       <div id="eventCard##EVENT-ID##" class="card" style="width: 18rem;">
@@ -23,11 +23,14 @@ function addEventCard(event){
               <h4 id="eventTitle##EVENT-ID##" style="text-align: center"></h4>
               <h6>Date of Event: <span id="eventDate##EVENT-ID##"></span></h6>
               <p id="eventDescription##EVENT-ID##" class="card-text"></p>
+              <div id="fullEventDescription##EVENT-ID##" style="display: none;"></div>
+              <div id="eventLocation##EVENT-ID##" style="display: none;"></div>
               <br>
               <div class="row text-center">
-                  <a  id="addEventButton##EVENT-ID##" href="#" class="btn btn-primary" style="margin: 10px">+</a>
-                  <a id="removeEventButton##EVENT-ID##" href="#" class="btn btn-primary" style="margin: 10px">-</a>
-                  <a id="moreInfoButton##EVENT-ID##" target="sblank" href="#" class="btn btn-primary" style="margin: 10px">More Info</a>
+                  <a  id="addEventButton##EVENT-ID##" href="#" class="addEventButton btn btn-primary" style="margin: 5px" onclick="addEventToBookmark('##EVENT-ID##'); return false;">+</a>
+                  <a id="removeEventButton##EVENT-ID##" href="#" class="removeEventButton btn btn-primary" style="margin: 5px">-</a>
+                  <a  id="eventRatingButton##EVENT-ID##" href="#" class="eventRatingButton btn btn-primary" style="margin: 5px" onclick="locationRatingButton('##EVENT-ID##'); return false;">*</a>
+                  <a id="moreInfoButton##EVENT-ID##" target="sblank" href="#" class="btn btn-primary" style="margin: 5px">More Info</a>
               </div>
           </div>
       </div>
@@ -40,77 +43,43 @@ function addEventCard(event){
   $("#eventTitle" + eventId).text(event.name.text);
   $("#eventDate" + eventId).text(moment(event.start.local).format('MMMM Do YYYY, h:mm a'));
   $("#eventDescription" + eventId).text(event.description.text.substring(0, 150) + "...");
+  $("#fullEventDescription" + eventId).text(event.description.text);
+  $("#eventLocation" + eventId).text(event.venue.name);
   $("#moreInfoButton" + eventId).attr("href", event.url);
+
 };
 
-function getEventByLatLong(latitude, longitude) {
+function addMapMarker(event,infowindow) {
+  let marker = new google.maps.Marker({
+    position: new google.maps.LatLng(event.venue.latitude, event.venue.longitude),
+    map: map,
+    title: event.name.text
+  });
 
+  google.maps.event.addListener(marker, 'click', (function (marker) {
+    return function () {
+      infowindow.setContent(event.name.text);
+      infowindow.open(map, marker);
+    }
+  })(marker));
+}
+
+function displayEventsByLatLong(latitude, longitude) {
   var eventURL = "https://www.eventbriteapi.com/v3/events/search/?token=" + auth + "&location.longitude=" + longitude + "&location.latitude=" + latitude + "&expand=venue";
-
   $.ajax({
     url: eventURL,
     method: "GET"
   }).then(function (response) {
-
-for(var i = 0; i < numOfEventsToDisplay; i++){
-    var currentEvent = response.events[i];
-    var eventTitle = currentEvent.name.text;
-    var eventDate = currentEvent.start.local;
-    var eventDescription = currentEvent.description.text;
-    var eventTwo = eventDescription.substring(0,150) + "...";
-    var eventImage = currentEvent.logo.original.url;
-
-    var eventLongitude = currentEvent.venue.longitude;
-    var eventLatitude = currentEvent.venue.latitude;
-
-    console.log(eventLatitude, eventLongitude);
-    calcLat = parseFloat(calcLat) + parseFloat(eventLatitude)
-    calcLong = parseFloat(calcLong) + parseFloat(eventLongitude)
-    
-    addEventCard(currentEvent);
-} 
-    console.log(calcLat)
-    console.log(calcLong)
-    avgLat = (calcLat / 10)
-    avgLong = (calcLong / 10)
-    calcLat = 0 
-    calcLong = 0
-    console.log(avgLat)
-    console.log(avgLong)
-    console.log(calcLat)
-    console.log(calcLong)
-
-
     events = response.events
-    var infowindow = new google.maps.InfoWindow();
-
-    var marker, i;
-    function markers() {
-      for (i = 0; i < events.length; i++) {
-        if (i < numOfEventsToDisplay) {
-          marker = new google.maps.Marker({
-            position: new google.maps.LatLng(events[i].venue.latitude, events[i].venue.longitude),
-            map: map,
-            title: events[i].name.text
-          });
-
-          google.maps.event.addListener(marker, 'click', (function (marker, i) {
-            return function () {
-              infowindow.setContent(events[i].name.text);
-              infowindow.open(map, marker);
-            }
-          })(marker, i));
-        }
-      }
+    let infowindow = new google.maps.InfoWindow();
+    for (var i = 0; i < numOfEventsToDisplay; i++) {
+      addEventCard(events[i]);
+      addMapMarker(events[i],infowindow);
     }
-    markers()
-    console.log(response);
   });
-
-
 };
 
-getEventByLatLong(latitude, longitude);
+displayEventsByLatLong(latitude, longitude);
 
 
 $("#search-name").on('keyup', function (e) {
@@ -148,108 +117,7 @@ $("#search-name").on('keyup', function (e) {
           mapTypeId: 'terrain'
         })
 
-
-        var auth = "S5UUTS2NYPECCKBYF5JY";
-        var bookmark = [];
-        function addEventCard(event){
-          
-
-          var eventId = event.id;
-          var template = `
-              <div id="eventCard##EVENT-ID##" class="card" style="width: 18rem;">
-                  <img id="eventPhoto##EVENT-ID##" class="card-img-top" src="" alt="Card image cap">
-                  <div class="card-body">
-                      <h4 id="eventTitle##EVENT-ID##" style="text-align: center"></h4>
-                      <h6>Date of Event: <span id="eventDate##EVENT-ID##"></span></h6>
-                      <p id="eventDescription##EVENT-ID##" class="card-text"></p>
-                      <br>
-                      <div class="row text-center">
-                          <a  id="addEventButton##EVENT-ID##" href="#" class="btn btn-primary" style="margin: 10px">+</a>
-                          <a id="removeEventButton##EVENT-ID##" href="#" class="btn btn-primary" style="margin: 10px">-</a>
-                          <a id="moreInfoButton##EVENT-ID##" target="sblank" href="#" class="btn btn-primary" style="margin: 10px">More Info</a>
-                      </div>
-                  </div>
-              </div>
-          `;
-          template = template.replace(/##EVENT-ID##/g, eventId);
-          $("#eventCardBody").append(template);
-          
-      
-          $("#eventPhoto" + eventId).attr("src", event.logo.original.url);
-          $("#eventTitle" + eventId).text(event.name.text);
-          $("#eventDate" + eventId).text(moment(event.start.local).format('MMMM Do YYYY, h:mm a'));
-          $("#eventDescription" + eventId).text(event.description.text.substring(0, 150) + "...");
-          $("#moreInfoButton" + eventId).attr("href", event.url);
-      };
-
-        function getEventByLatLong(latitude, longitude) {
-
-          var eventURL = "https://www.eventbriteapi.com/v3/events/search/?token=" + auth + "&location.longitude=" + longitude + "&location.latitude=" + latitude + "&expand=venue";
-
-          $.ajax({
-            url: eventURL,
-            method: "GET"
-          }).then(function (response) {
-
-        for(var i = 0; i < numOfEventsToDisplay; i++){
-            var currentEvent = response.events[i];
-            var eventTitle = currentEvent.name.text;
-            var eventDate = currentEvent.start.local;
-            var eventDescription = currentEvent.description.text;
-            var eventTwo = eventDescription.substring(0,150) + "...";
-            var eventImage = currentEvent.logo.original.url;
-
-            var eventLongitude = currentEvent.venue.longitude;
-            var eventLatitude = currentEvent.venue.latitude;
-
-            console.log(eventLatitude, eventLongitude);
-            calcLat = parseFloat(calcLat) + parseFloat(eventLatitude)
-            calcLong = parseFloat(calcLong) + parseFloat(eventLongitude)
-            
-            addEventCard(currentEvent);
-        } 
-            console.log(calcLat)
-            console.log(calcLong)
-            avgLat = (calcLat / 10)
-            avgLong = (calcLong / 10)
-            calcLat = 0 
-            calcLong = 0
-            console.log(avgLat)
-            console.log(avgLong)
-            console.log(calcLat)
-            console.log(calcLong)
-
-
-            events = response.events
-            var infowindow = new google.maps.InfoWindow();
-
-            var marker, i;
-            function markers() {
-              for (i = 0; i < events.length; i++) {
-                if (i < numOfEventsToDisplay) {
-                  marker = new google.maps.Marker({
-                    position: new google.maps.LatLng(events[i].venue.latitude, events[i].venue.longitude),
-                    map: map,
-                    title: events[i].name.text
-                  });
-
-                  google.maps.event.addListener(marker, 'click', (function (marker, i) {
-                    return function () {
-                      infowindow.setContent(events[i].name.text);
-                      infowindow.open(map, marker);
-                    }
-                  })(marker, i));
-                }
-              }
-            }
-            markers()
-            console.log(response);
-          });
-
-
-        };
-
-        getEventByLatLong(latitude, longitude);
+        displayEventsByLatLong(latitude, longitude);
 
       }
     })
@@ -290,101 +158,7 @@ $("#search").on("click", function () {
         mapTypeId: 'terrain'
       })
 
-      var auth = "S5UUTS2NYPECCKBYF5JY";
-      var bookmark = [];
-      function addEventCard(event){
-        $("#eventCardBody").empty();
-        var eventId = event.id;
-        var template = `
-            <div id="eventCard##EVENT-ID##" class="card" style="width: 18rem;">
-                <img id="eventPhoto##EVENT-ID##" class="card-img-top" src="" alt="Card image cap">
-                <div class="card-body">
-                    <h4 id="eventTitle##EVENT-ID##" style="text-align: center"></h4>
-                    <h6>Date of Event: <span id="eventDate##EVENT-ID##"></span></h6>
-                    <p id="eventDescription##EVENT-ID##" class="card-text"></p>
-                    <br>
-                    <div class="row text-center">
-                        <a  id="addEventButton##EVENT-ID##" href="#" class="btn btn-primary" style="margin: 10px">+</a>
-                        <a id="removeEventButton##EVENT-ID##" href="#" class="btn btn-primary" style="margin: 10px">-</a>
-                        <a id="moreInfoButton##EVENT-ID##" target="sblank" href="#" class="btn btn-primary" style="margin: 10px">More Info</a>
-                    </div>
-                </div>
-            </div>
-        `;
-        template = template.replace(/##EVENT-ID##/g, eventId);
-        $("#eventCardBody").append(template);
-        
-    
-        $("#eventPhoto" + eventId).attr("src", event.logo.original.url);
-        $("#eventTitle" + eventId).text(event.name.text);
-        $("#eventDate" + eventId).text(moment(event.start.local).format('MMMM Do YYYY, h:mm a'));
-        $("#eventDescription" + eventId).text(event.description.text.substring(0, 150) + "...");
-        $("#moreInfoButton" + eventId).attr("href", event.url);
-    };
-
-      function getEventByLatLong(latitude, longitude) {
-
-        var eventURL = "https://www.eventbriteapi.com/v3/events/search/?token=" + auth + "&location.longitude=" + longitude + "&location.latitude=" + latitude + "&expand=venue";
-
-        $.ajax({
-          url: eventURL,
-          method: "GET"
-        }).then(function (response) {
-      console.log(response);
-
-      for(var i = 0; i < numOfEventsToDisplay; i++){
-          var currentEvent = response.events[i];
-          var eventTitle = currentEvent.name.text;
-          var eventDate = currentEvent.start.local;
-          var eventDescription = currentEvent.description.text;
-          var eventTwo = eventDescription.substring(0,150) + "...";
-          var eventImage = currentEvent.logo.original.url;
-
-          var eventLongitude = currentEvent.venue.longitude;
-          var eventLatitude = currentEvent.venue.latitude;
-
-          console.log(eventLatitude, eventLongitude);
-          calcLat = parseFloat(calcLat) + parseFloat(eventLatitude)
-          calcLong = parseFloat(calcLong) + parseFloat(eventLongitude)
-          addEventCard(currentEvent);
-      } 
-      avgLat = calcLat / 10;
-      avgLong = calcLong / 10;
-      calcLat = 0 
-      calcLong = 0
-      console.log(calcLat)
-      console.log(calcLong)
-          events = response.events
-          var infowindow = new google.maps.InfoWindow();
-
-          var marker, i;
-          function markers() {
-            for (i = 0; i < events.length; i++) {
-              if (i < numOfEventsToDisplay) {
-                marker = new google.maps.Marker({
-                  position: new google.maps.LatLng(events[i].venue.latitude, events[i].venue.longitude),
-                  map: map,
-                  title: events[i].name.text
-                });
-
-                google.maps.event.addListener(marker, 'click', (function (marker, i) {
-                  return function () {
-                    infowindow.setContent(events[i].name.text);
-                    infowindow.open(map, marker);
-                  }
-                })(marker, i));
-              }
-            }
-          }
-          markers()
-          console.log(response);
-
-        });
-
-
-      };
-
-      getEventByLatLong(latitude, longitude);
+      displayEventsByLatLong(latitude, longitude);
 
 
     },
@@ -403,6 +177,4 @@ function initMap() {
     center: myLatLng,
     zoom: 12,
     mapTypeId: 'terrain'
-  })
-
-};
+  })};
