9adf3ae27297cbc4564d783e408403e5ce0ba03f
Maci Slenes <maci.slenes@gmail.com>
2019-08-17 09:22:20 -0700

    added final js code

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -105,7 +105,7 @@ function addEventCardFromDB(event){
     </li>
 `;
   template = template.replace(/##EVENT-ID##/g, eventId);
-  $(".uk-slideshow-items").append(template);
+  $(".uk-slider-items").append(template);
 
   // instead of this information coming from our ajax call to EventBrite this is coming from Firebase
   $("#eventPhoto" + eventId).attr("src", event.eventPhoto);
@@ -381,5 +381,47 @@ function removeEventFromBookmark(eventId){
        ajaxStop: function() { $body.removeClass("loading"); }    
   });
 
+  $("#seaButton").on("click", function(){
+    
+    localStorage.setItem("latitude", 47.6062);
+    localStorage.setItem("longitude", -122.3321);
+    console.log(localStorage)
+    window.location = "./maps.html";
+});
 
+$("#sanFranButton").on("click", function(){
+    
+    localStorage.setItem("latitude", 37.7749);
+    localStorage.setItem("longitude", -122.4194);
+    console.log(localStorage)
+    window.location = "./maps.html";
+});
+$("#parisButton").on("click", function(){
+    
+    localStorage.setItem("latitude", 48.8566);
+    localStorage.setItem("longitude", 2.3522);
+    console.log(localStorage)
+    window.location = "./maps.html";
+});
+$("#athensButton").on("click", function(){
+    
+    localStorage.setItem("latitude", 37.9838);
+    localStorage.setItem("longitude", 23.7275);
+    console.log(localStorage)
+    window.location = "./maps.html";
+});
+$("#madridButton").on("click", function(){
+    
+    localStorage.setItem("latitude", 40.4168);
+    localStorage.setItem("longitude", -3.7038);
+    console.log(localStorage)
+    window.location = "./maps.html";
+});
+$("#hKButton").on("click", function(){
+    
+    localStorage.setItem("latitude", 22.3193);
+    localStorage.setItem("longitude", 114.1694);
+    console.log(localStorage)
+    window.location = "./maps.html";
+});
   
